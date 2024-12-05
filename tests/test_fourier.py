9adf7f6caa219cf365897f2303ece08016e64c9b
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2024-12-05 20:19:50 +0000

    Created tests!

--- tests/test_fourier.py
+++ tests/test_fourier.py
@@ -1,36 +1,37 @@
 import numpy as np
 import pytest
+from numpy.testing import assert_allclose
+
 from pyspecdenoise.fourier import fourier_smoothing
 
-TEST_FILE = 'SN2004gq_2004-12-12_07-12-00_FLWO-1.5m_FAST_CfA-Stripped_clipped.flm'
-CHECK_FILE = 'smoothed.txt'
+TEST_FILE = (
+    "tests/SN2004gq_2004-12-12_07-12-00_FLWO-1.5m_FAST_CfA-Stripped.flm"
+)
+CHECK_FILE = "tests/smoothed.txt"
+
 
 @pytest.fixture
 def load_input_spec():
-    with open(TEST_FILE) as newfile:
-        lines = newfile.readlines()
-        i = 0
-        for line in lines:
-            if line[2] != " ":
-                break
-            i += 1
-
-    wave, flux = np.loadtxt(TEST_FILE, usecols=range(2), skiprows=i, unpack=True)
+    wave, flux = np.loadtxt(TEST_FILE, usecols=range(2), unpack=True)
 
     return wave, flux
 
+
 @pytest.fixture
 def load_comparison_spec():
-    wave, flux, flux_smooth = np.loadtxt(CHECK_FILE)
+    wave, flux, flux_smooth = np.loadtxt(CHECK_FILE, unpack=True)
     return wave, flux, flux_smooth
 
-def test_fourier_smoothing():
+
+def test_fourier_smoothing(load_input_spec, load_comparison_spec):
     input_wavelength, input_flux = load_input_spec
 
     wlen, new_flux = fourier_smoothing(
         input_wavelength / (1 + 0.0065), input_flux / np.mean(input_flux)
-    ) # Divide by the mean so that all spectra are "normalised".
+    )  # Divide by the mean so that all spectra are "normalised".
 
-    x = load_comparison_spec
+    wave, flux, flux_smooth = load_comparison_spec
 
-    assert x == np.stack((wlen, input_flux, new_flux))
+    assert_allclose(wlen, wave)
+    assert_allclose(input_flux, flux)
+    assert_allclose(new_flux, flux_smooth)
